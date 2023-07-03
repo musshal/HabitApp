28e46aaa1ff7d3f16f621d16096685fe3ec294ca
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-03 22:53:10 +0700

    refactor: define room and prepopulate db

--- app/src/main/java/com/dicoding/habitapp/data/HabitDatabase.kt
+++ app/src/main/java/com/dicoding/habitapp/data/HabitDatabase.kt
@@ -1,8 +1,14 @@
 package com.dicoding.habitapp.data
 
 import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.dicoding.habitapp.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
@@ -11,6 +17,8 @@ import java.io.IOException
 import java.io.InputStreamReader
 
 //TODO 3 : Define room database class and prepopulate database using JSON
+
+@Database(entities = [Habit::class], version = 1, exportSchema = false)
 abstract class HabitDatabase : RoomDatabase() {
 
     abstract fun habitDao(): HabitDao
@@ -21,7 +29,26 @@ abstract class HabitDatabase : RoomDatabase() {
         private var INSTANCE: HabitDatabase? = null
 
         fun getInstance(context: Context): HabitDatabase {
-            throw NotImplementedError("Not yet implemented")
+            return synchronized(this) {
+                INSTANCE ?: Room
+                    .databaseBuilder(
+                        context.applicationContext,
+                        HabitDatabase::class.java,
+                        "habits.db"
+                    )
+                    .addCallback(object : Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            INSTANCE?.let { habitDatabase ->
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    val habitDao = habitDatabase.habitDao()
+                                    fillWithStartingData(context, habitDao)
+                                }
+                            }
+                        }
+                    })
+                    .build()
+            }
         }
 
         private fun fillWithStartingData(context: Context, dao: HabitDao) {
