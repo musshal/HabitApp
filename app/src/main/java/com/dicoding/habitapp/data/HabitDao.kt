98980a7c4eb315d11dc07fbfd8c42f51aa51358e
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-03 22:35:05 +0700

    refactor: define dao

--- app/src/main/java/com/dicoding/habitapp/data/HabitDao.kt
+++ app/src/main/java/com/dicoding/habitapp/data/HabitDao.kt
@@ -2,20 +2,33 @@ package com.dicoding.habitapp.data
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.sqlite.db.SupportSQLiteQuery
 
 //TODO 2 : Define data access object (DAO)
+
+@Dao
 interface HabitDao {
 
+    @Query("SELECT * FROM habits")
     fun getHabits(query: SupportSQLiteQuery): DataSource.Factory<Int, Habit>
 
+    @Query("SELECT * FROM habits WHERE id = :habitId")
     fun getHabitById(habitId: Int): LiveData<Habit>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertHabit(habit: Habit): Long
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg habits: Habit)
 
+    @Delete
     fun deleteHabit(habits: Habit)
 
+    @Query("SELECT * FROM habits WHERE priorityLevel = :level ORDER BY RANDOM() LIMIT 1")
     fun getRandomHabitByPriorityLevel(level: String): LiveData<Habit>
 }
