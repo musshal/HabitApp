bb7754e8c24bb25d8d3137c19abc1c5485ba5165
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-04 05:00:02 +0700

    fix: fix bug

--- app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitActivity.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitActivity.kt
@@ -34,14 +34,22 @@ class RandomHabitActivity : AppCompatActivity() {
         val factory = ViewModelFactory.getInstance(this)
         val viewModel = ViewModelProvider(this, factory).get(RandomHabitViewModel::class.java)
 
-        viewModel.priorityLevelHigh.observe(this) {
-            adapter.submitData(RandomHabitAdapter.PageType.HIGH, it)
+        viewModel.priorityLevelHigh.observe(this) { priorityLevel ->
+            priorityLevel?.let {
+                adapter.submitData(RandomHabitAdapter.PageType.HIGH, it)
+            }
         }
-        viewModel.priorityLevelMedium.observe(this) {
-            adapter.submitData(RandomHabitAdapter.PageType.MEDIUM, it)
+
+        viewModel.priorityLevelMedium.observe(this) { priorityLevel ->
+            priorityLevel?.let {
+                adapter.submitData(RandomHabitAdapter.PageType.MEDIUM, it)
+            }
         }
-        viewModel.priorityLevelLow.observe(this) {
-            adapter.submitData(RandomHabitAdapter.PageType.LOW, it)
+
+        viewModel.priorityLevelLow.observe(this) { priorityLevel ->
+            priorityLevel?.let {
+                adapter.submitData(RandomHabitAdapter.PageType.LOW, it)
+            }
         }
 
     }
