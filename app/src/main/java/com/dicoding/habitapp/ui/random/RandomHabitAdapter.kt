040f3160d15e142bdfff881924598a801541b32c
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-04 05:00:24 +0700

    refactor: code refactor

--- app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
@@ -3,6 +3,9 @@ package com.dicoding.habitapp.ui.random
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
@@ -41,7 +44,25 @@ class RandomHabitAdapter(
         //TODO 14 : Create view and bind data to item view
 
         fun bind(pageType: PageType, pageData: Habit) {
+            val pagerTvTitle = itemView.findViewById<TextView>(R.id.pager_tv_title)
+            val pagerTvStartTime = itemView.findViewById<TextView>(R.id.pager_tv_start_time)
+            val pagerIvPriorityLevel = itemView.findViewById<ImageView>(R.id.pager_priority_level)
+            val pagerTvCountTime = itemView.findViewById<TextView>(R.id.pager_tv_minutes)
+            val btnOpenCountDown = itemView.findViewById<Button>(R.id.btn_open_count_down)
 
+            pagerTvTitle.text = pageData.title
+            pagerTvStartTime.text = pageData.startTime
+            pagerTvCountTime.text = pageData.minutesFocus.toString()
+
+            when (pageType) {
+                PageType.HIGH -> pagerIvPriorityLevel.setImageResource(R.drawable.ic_priority_high)
+                PageType.MEDIUM -> pagerIvPriorityLevel.setImageResource(R.drawable.ic_priority_medium)
+                PageType.LOW -> pagerIvPriorityLevel.setImageResource(R.drawable.ic_priority_low)
+            }
+
+            btnOpenCountDown.setOnClickListener {
+                onClick(pageData)
+            }
         }
     }
 }
